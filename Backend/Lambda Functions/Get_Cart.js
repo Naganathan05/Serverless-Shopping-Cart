00ce1abf65c6b96dbb77101bcfda5f1a2f97375a
Naganathan05 <umaiyalramesh@gmail.com>
2024-01-28 12:51:25 +0530

    Added Get_Cart Lambda Function

--- Backend/Lambda Functions/Get_Cart.js
+++ Backend/Lambda Functions/Get_Cart.js
@@ -5,12 +5,54 @@ const AWS = require('aws-sdk')
 AWS.config.update({ region: "us-east-1" });
 
 // Create the DynamoDB Service Object
-var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { BatchGetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-exports.handler = function(event, context, callback){
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async function(event, context, callback){
     let responseCode = 200
     let responseBody = ""
-    const params = {
-        
+    let userIdValue;
+
+    const cookieHeader = event.headers['cookie'] || event.headers['Cookie'];
+    if (cookieHeader) {
+    const userId = cookieHeader.split(';').find(cookie => cookie.includes('userId'));
+    if (userId) {
+        const userIdValue = userId.split('=')[1];
+        console.log('User ID:', userIdValue);
+        // Now you can use userIdValue in your Lambda function
+    } else {
+        console.log('User ID not found in cookie');
     }
+    } else {
+    console.log('Cookie header not found in the request');
+    }
+
+    const command = new BatchGetCommand({
+        RequestItems: {
+            Anonymous_Cart: {
+                Keys: [
+                    {
+                        User_Id: userIdValue
+                    }
+                ],
+                ProjectionExpression: "Product_Id, Product_Name, Quantity, Amount"
+            }
+        }
+    });
+
+    const result = await docClient.send(command)
+    console.log(result.Responses["Anonymous_Cart"])
+    responseBody = result.Responses["Anonymous_Cart"];
+    const response = {
+        statusCode: responseCode,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(responseBody)
+    }
+    return response;
 }
